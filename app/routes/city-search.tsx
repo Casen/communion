39ab120f027658d4c635498cdaa02983da8123d5
Casen Davis <cdubd10@gmail.com>
2022-12-01 16:21:55 -0800

    fix reach ui problems for now

--- app/routes/city-search.tsx
+++ app/routes/city-search.tsx
@@ -1,34 +1,27 @@
 import { useFetcher } from "@remix-run/react";
 import { useMemo, useEffect } from "react";
 import debounce from 'lodash.debounce'
-import {
-  Combobox,
-  ComboboxInput,
-  ComboboxPopover,
-  ComboboxList,
-  ComboboxOption,
-} from "@reach/combobox";
-import combostyles from "@reach/combobox/styles.css";
+import { Fragment, useState } from 'react'
+import { Combobox, Transition } from '@headlessui/react'
+import { CheckIcon, ChevronUpDownIcon, MagnifyingGlassIcon } from '@heroicons/react/20/solid'
 
-export function links() {
-  return [
-    {
-      rel: "stylesheet",
-      href: combostyles,
-    },
-  ];
+interface City {
+  id: string
+  name: string
+  regionCode: string
+  countryCode: string
 }
 
-export default function Index() {
+
+export default function CitySearch() {
+  const [selected, selectCity] = useState<City>()
+  const [query, setQuery] = useState('')
+
   const cities = useFetcher()
-  console.log('got cities: ', cities)
 
   const search = (event: any) => {
+    console.log('searching cities')
     cities.submit(event.target.form)
-  }
-
-  const selectCity =(city: any) => {
-    console.log(city)
   }
 
   const debouncedSearch = useMemo(
@@ -43,43 +36,84 @@ export default function Index() {
     }
   });
 
-  return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <cities.Form method="get" action="/api/cities" autoComplete="off" role="presentation">
-      <Combobox aria-label="Cities">
-        <div>
-          <ComboboxInput
-            name="q"
-            onChange={debouncedSearch}
-          />
-          {cities.state === "submitting" ? (
-            <p>loading..</p>
-          ) : null}
-        </div>
 
-        {cities.data ? (
-          <ComboboxPopover className="shadow-popup">
-            {cities.data.error ? (
-              <p>Failed to load cities :(</p>
-            ) : cities.data.length ? (
-              <ComboboxList>
-                {cities.data.map((city: any) => (
-                  <ComboboxOption
+
+  return (
+    <div className="fixed top-16 w-72">
+      <cities.Form method="get" action="/api/cities" autoComplete="off" role="presentation">
+      <Combobox value={selected} onChange={selectCity}>
+        <div className="relative mt-1">
+          <div className="relative w-full cursor-default overflow-hidden rounded-lg bg-white text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm">
+            <Combobox.Input
+              name = "q"
+              placeholder="Los Angeles"
+              className="w-full border-none py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:ring-0"
+              displayValue={(city: City) => city ? `${city?.name}, ${city?.regionCode}, ${city?.countryCode}` : ''}
+              onChange={debouncedSearch}
+            />
+            <Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
+              <MagnifyingGlassIcon
+                className="h-5 w-5 text-gray-400"
+                aria-hidden="true"
+              />
+            </Combobox.Button>
+          </div>
+          <Transition
+            as={Fragment}
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+            afterLeave={() => setQuery('')}
+          >
+            <Combobox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+              {(!cities.data || cities.data?.length === 0) && query !== '' ? (
+                <div className="relative cursor-default select-none py-2 px-4 text-gray-700">
+                  Nothing found.
+                </div>
+              ) : null }
+              {(cities.state === "submitting") ? (
+                <div className="relative cursor-default select-none py-2 px-4 text-gray-700">
+                  Loading...
+                </div>
+              ) : null }
+              {(cities.data || []).map((city: City) => (
+                  <Combobox.Option
                     key={city.id}
-                    value={`${city.name}, ${city.regionCode}, ${city.countryCode}`}
-                    onClick={(event)=> selectCity(city)}
-                  />
-                ))}
-              </ComboboxList>
-            ) : (
-              <span>No results found</span>
-            )}
-          </ComboboxPopover>
-        ) : null}
+                    className={({ active }) =>
+                      `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                        active ? 'bg-teal-600 text-white' : 'text-gray-900'
+                      }`
+                    }
+                    value={city}
+                  >
+                    {({ selected, active }) => (
+                      <>
+                        <span
+                          className={`block truncate ${
+                            selected ? 'font-medium' : 'font-normal'
+                          }`}
+                        >
+                          {`${city?.name}, ${city?.regionCode}, ${city?.countryCode}`}
+                        </span>
+                        {selected ? (
+                          <span
+                            className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
+                              active ? 'text-white' : 'text-teal-600'
+                            }`}
+                          >
+                            <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                          </span>
+                        ) : null}
+                      </>
+                    )}
+                  </Combobox.Option>
+                ))
+              }
+            </Combobox.Options>
+          </Transition>
+        </div>
       </Combobox>
     </cities.Form>
-     
     </div>
-  );
+  )
 }
